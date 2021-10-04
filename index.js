6c6ae2e34f8da687f2bbeccdcb1caa8033418ed4
Drummir <ivan-syomin@mail.ru>
2021-10-04 19:28:36 +0300

    minor upd animation

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ ScrollMagicPluginGsap(ScrollMagic, TweenMax);
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.to(`body`, {
-  duration: 1,
+  duration: 5,
   scrollTrigger: {
     trigger: `.animation`,
     start: `top center`,
@@ -32,18 +32,18 @@ gsap.to(`body`, {
 
 const a = gsap.timeline();
 
-gsap.to(`.front`, {
-  x: `+200vw`,
-  duration: 5,
-  repeat: -1,
-  ease: `none`,
-});
-gsap.to(`.back`, {
-  x: `-200vw`,
-  duration: 5,
-  repeat: -1,
-  ease: `none`,
-});
+// gsap.to(`.front`, {
+//   x: `+200vw`,
+//   duration: 5,
+//   repeat: -1,
+//   ease: `none`,
+// });
+// gsap.to(`.back`, {
+//   x: `-200vw`,
+//   duration: 5,
+//   repeat: -1,
+//   ease: `none`,
+// });
 
 a.to(`.description-1`, {
   y: `-20vh`,
@@ -52,27 +52,44 @@ a.to(`.description-1`, {
   ease: `none`,
 });
 
-ScrollTrigger.create({
-  trigger: `.photo-2`,
-  scroller: `body`,
-  animation: a,
-  start: `top bottom`,
-  end: `top 60%`,
-  scrub: true,
-  pin: `.description-1`,
-  toggleActions: `restart none reverse none`,
-  // markers: {
-  //   fontSize: `20px`,
-  //   indent: 100,
-  // },
+const lastIndex = gsap.utils.toArray('.photo-wrapper').length - 1;
+const tween = gsap.timeline();
+const controller = new ScrollMagic.Controller();
+
+document.querySelectorAll('.photo-wrapper').forEach((x, i) => {
+  if (i !== 0) {
+    x.style.top = `${110 * i}vh`;
+  }
 });
 
-// Костыль
+gsap.utils.toArray('.photo-wrapper').forEach((container, i) => {
+  if (i < lastIndex) {
+    console.log(`description ${i + 1}, photo ${i + 2}`);
+    ScrollTrigger.create({
+      trigger: `.photo-${i + 2}`,
+      scroller: `body`,
+      animation: a,
+      start: `top bottom`,
+      end: `top 60%`,
+      scrub: true,
+      pin: `.description-${i + 1}`,
+      toggleActions: `restart none reverse none`,
+      // markers: {
+      //   fontSize: `20px`,
+      //   indent: 100,
+      // },
+    });
+  }
+  if (i !== 0) {
+    // Костыль
+    console.log(`container ${i} of ${lastIndex}`);
+    tween.add(gsap.to(container, { top: 0, duration: 1 }));
 
-const tween = gsap.timeline();
-tween.add(gsap.to(`.photo-wrapper-2`, { top: 0, duration: 1 }));
+    // End Костыль
+  }
+});
 
-const controller = new ScrollMagic.Controller();
+console.log(tween);
 
 const scene = new ScrollMagic.Scene({
   triggerElement: `.animation`,
@@ -82,6 +99,22 @@ const scene = new ScrollMagic.Scene({
 })
   .setTween(tween)
   .setPin(`.animation`)
+  .addIndicators({
+    fontSize: `20px`,
+    indent: 100,
+  })
   .addTo(controller);
-
-// End Костыль
+// ScrollTrigger.create({
+//   trigger: `.photo-2`,
+//   scroller: `body`,
+//   animation: a,
+//   start: `top bottom`,
+//   end: `top 60%`,
+//   scrub: true,
+//   pin: `.description-1`,
+//   toggleActions: `restart none reverse none`,
+//   // markers: {
+//   //   fontSize: `20px`,
+//   //   indent: 100,
+//   // },
+// });
